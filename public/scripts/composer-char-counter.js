db24e8095ef4397c83c4f3b249026d678e63ea0a
Dickson Yu <dickson.yu@email.com>
2021-12-16 23:14:17 -0500

    Cleaned up code, added comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,19 @@
 // DOM Load
-
 $(document).ready(function() {
 
+  // Character counter
   $('textarea').on('input', function() {
-
     const tweetLimit = 140;
     let tweetLength = $(this).val().length;
     let characterCounter = $(this).siblings('.counter');
 
+    // Tweet length check
     if (tweetLength > tweetLimit) {
       characterCounter.addClass('tweetTooLong');
     } else if (tweetLength <= tweetLimit) {
       characterCounter.removeClass('tweetTooLong');
     }
+    // Character counter update everytime you type
     characterCounter.text(tweetLimit - tweetLength);
   });
-
 });
